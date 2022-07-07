6572b74f04e6e2aba5fa5038d38be49bc89471ca
merajhassan <merajafnan@gmail.com>
2022-07-07 21:33:31 +0530

    Solution Added

--- Move Zeroes.py
+++ Move Zeroes.py
@@ -1,35 +1,21 @@
 nums = [0,1,0,3,12]
-
-last = len(nums) - 1
+pos = []
+for i in range(len(nums)):
+    if nums[i] == 0:
+        pos.append(i)
+print(pos)
+leng = len(pos)
 k = 0
+for i in pos:
+    nums.pop(i-k)
+    k += 1
+print(nums)
+for l in range(leng):
+    nums.append(0)
+print(nums)
 
-d = []
 
-for i in nums:
-    if i != 0:
-        d.append(i)
-    else:
-        k += 1
-print(d)
-print(k)
-for j in range(k):
-    d.append(0)
-print(d)
-#
-# k = 0
-# d = []
-#
-# for i in nums:
-#     if i != 0:
-#         d.append(i)
-#     else:
-#         k = k+1
-# for j in range(k):
-#     d.append(0)
-# nums = d
-#
-# print(nums)
-#
-#
+
+
 
 
