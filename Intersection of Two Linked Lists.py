e229fa7d70b74571c49a43a4b8df38307b5d3e17
merajhassan <merajafnan@gmail.com>
2022-07-10 11:31:07 +0530

    Solution Added

---
+++ Intersection of Two Linked Lists.py
@@ -0,0 +1,22 @@
+# Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+# For example, the following two linked lists begin to intersect at node c1:
+# https://assets.leetcode.com/uploads/2021/03/05/160_example_1_1.png
+# Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+# Output: Intersected at '8'
+# Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+# From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+
+        l1,r1 = headA,headB
+        while l1 != r1:
+            l1 = l1.next if l1 else headB
+            r1 = r1.next if r1 else headA
+        return l1
